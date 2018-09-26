b1fce2bf2ebcb880a8c33345ac0bdda38ac530f1
Abhishek Dwivedi <abhishek.d@techaspect.com>
2018-09-26 14:59:57 +0530

    Added Source Code For JavaScript Left Rotation

---
+++ Day_5/leftrotation.js
@@ -0,0 +1,43 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+
+
+function main() {
+    const nd = readLine().split(' ');
+
+    const n = parseInt(nd[0], 10);
+
+    const d = parseInt(nd[1], 10);
+
+    const a = readLine().split(' ').map(aTemp => parseInt(aTemp, 10));
+    
+    var rotated = new Array();
+        for(var i=0; i<n; i++){
+            rotated[(i+n-d)%n] = a[i];
+        }
+    var str = rotated.join(' ');
+    console.log(str);
+    
+}
