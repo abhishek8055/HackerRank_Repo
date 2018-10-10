d59861de609e2d3e51243c5cc158b09ce0f5f625
Abhishek Dwivedi <abhishek.d@tadigital.com>
2018-10-10 18:31:18 +0530

    Added Source Code For JavaScript Challenge Pangrams

---
+++ Day_13/pangrams.js
@@ -0,0 +1,54 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+// Complete the pangrams function below.
+function pangrams(s) {
+    var str = s.toLowerCase();
+    var set = new Set();
+    str.split('').forEach((alpha)=>{
+        if(alpha != ' ') {
+            set.add(alpha);
+        }
+    });
+    console.log(set);
+    if(set.size == 26) {
+        return "pangram"
+    } else {
+        return "not pangram"
+    }
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const s = readLine();
+
+    let result = pangrams(s);
+
+    ws.write(result + "\n");
+
+    ws.end();
+}
