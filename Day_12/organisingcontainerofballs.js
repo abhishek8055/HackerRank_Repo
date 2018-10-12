2d1baee0ec3ba243a95df38bd2fb173f0bbf462b
Abhishek Dwivedi <abhishek.d@tadigital.com>
2018-10-12 14:18:20 +0530

    Added Source Code For JavaScript Challenge Organising Container Of Balls

--- Day_12/organisingcontainerofballs.js
+++ Day_12/organisingcontainerofballs.js
@@ -24,59 +24,87 @@ function readLine() {
     return inputString[currentLine++];
 }
 
-
-
-// Complete the organizingContainers function below.
-function organizingContainers(container) {
-    const n = container.length;
-    const a = [n];
-    const b = [n];
-    for( var i=0;i<n;i++) {
-        a[i]=0;
-        b[i]=0;
-    }
-    for(var i=0; i<n; i++) {
-        for(var j=0; j<n; j++) {
-            let ball = container[i][j];
-            a[i]+= ball;
-            b[j]+= ball;
-        }
-    }
-    var pts = "Possible";
+// Complete the biggerIsGreater function below.
+function biggerIsGreater(w) {
+    const word = w.split('');
     
-    for(var i=0;i<n;i++) {
-        var j=0;
-        for(j=i;j<n;j++) {
-            if(a[i] == b[j]) {
-                var temp = b[j];
-                b[j] = b[i];
-                b[i] = temp;
-                break;
-            }
-        }
-        if(j==n) {
-            pts = "Impossible";
+    //CONDITION 1
+    //IF WORD CONSIST OF A SINGLE LETTER THEN PRINT "NO ANSWER"
+    let first = word[0];
+    let flag = false;
+    for(let i=1; i<word.length; i++) {
+        if(word[i]!=first){
+            flag = true;
             break;
         }
     }
-    return pts;
+    if(!flag) {
+        return "no answer";
+    }
+    
+    //CONDITION 2
+    //IF WORD IS ALREADY THE LARGEST, PRINT "NO ANSWER"
+    let largest = new Array(...word);
+    largest.sort();
+    largest.reverse();
+    
+    let status = true;
+    for (var i = 0; i < word.length; ++i) {
+        if (word[i] != largest[i]) {
+            status = false;
+            break;
+        }
+    }
+    if(status) {
+        return "no answer";
+    }
+    
+    //ELSE
+    //FIND THE CLOSEST LARGE WORD
+    let size = word.length;
+    let index;
+    for(let i=size-1; i>=0; i--) { 
+        let swapped = false;
+        for(let j=i-1; j>=0; j--) {
+            if(word[i] > word[j]) {
+                let temp = word[i]; 
+                word[i] = word[j];
+                word[j] = temp;
+                index = j;
+                swapped = true;
+                break;
+            }
+        }
+        if(swapped) {
+            break;
+        }
+    }
+    
+    for(let i=index+1; i<size-1; i++) {
+        for(let j=i+1; j<size; j++) {
+            if(word[i] > word[j]) {
+                let temp = word[i]; 
+                word[i] = word[j];
+                word[j] = temp;
+            }
+        }
+    }
+    
+    let str = word.toString();
+    const n = str.replace(/,/g, '');
+    return n;
+
 }
 
 function main() {
     const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
 
-    const q = parseInt(readLine(), 10);
+    const T = parseInt(readLine(), 10);
 
-    for (let qItr = 0; qItr < q; qItr++) {
-        const n = parseInt(readLine(), 10);
+    for (let TItr = 0; TItr < T; TItr++) {
+        const w = readLine();
 
-        let container = Array(n);
-
-        for (let i = 0; i < n; i++) {
-            container[i] = readLine().split(' ').map(containerTemp => parseInt(containerTemp, 10));
-        }
-
-        let result = organizingContainers(container);
+        let result = biggerIsGreater(w);
 
         ws.write(result + "\n");
     }
