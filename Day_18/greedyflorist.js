52e806c1695b2518b1b43e929dafa05e1a936ae7
Abhishek Dwivedi <abhishek.d@tadigital.com>
2018-10-17 11:54:39 +0530

    Added Source Code For JavaScript Challenge Greedy Florist

---
+++ Day_18/greedyflorist.js
@@ -0,0 +1,58 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+// Complete the getMinimumCost function below.
+function getMinimumCost(k, c) {
+    var n = c.length;
+    var multiply = 0;
+    var total = 0;
+    c.sort(function(a, b) {
+        return Number(b) - Number(a);
+    });
+    for (let i = 0; i<n; i++) {
+        if(i%k == 0) multiply++;
+        total += c[i] * multiply;
+    }
+    return total;
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const nk = readLine().split(' ');
+
+    const n = parseInt(nk[0], 10);
+
+    const k = parseInt(nk[1], 10);
+
+    const c = readLine().split(' ').map(cTemp => parseInt(cTemp, 10));
+
+    const minimumCost = getMinimumCost(k, c);
+
+    ws.write(minimumCost + '\n');
+
+    ws.end();
+}
